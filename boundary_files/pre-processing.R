19170fbf76218f7c487c1be9d010dfdf6ad3e3b0
Jhumbl <jackhumble121@gmail.com>
2021-08-27 18:59:04 +0100

    initial commit

--- boundary_files/pre-processing.R
+++ boundary_files/pre-processing.R
@@ -18,25 +18,31 @@ library(dplyr)
 
 url <- "https://data.london.gov.uk/download/statistical-gis-boundary-files-london/9ba8c833-6370-4b11-abdc-314aa020d5e0/statistical-gis-boundaries-london.zip"
 download.file(url, destfile="temp_data.zip")
-unzip("temp.zip", exdir = "temp_data")
+unzip("temp_data.zip", exdir = "temp_data")
 
 #------------- Read In Data Filter for Westminster --------------#
 
-oa <- read_sf("temp_data/statistical-gis-boundaries-london/ESRI/OA_2011_London_gen_MHW.shp") %>%
+path <- file.path("temp_data", "statistical-gis-boundaries-london", "ESRI")
+
+oa <- read_sf(file.path(path,"OA_2011_London_gen_MHW.shp")) %>%
   dplyr::filter(LAD11NM == "Westminster") %>%
   st_transform(4326)
 
-lsoa <- read_sf("temp_data/statistical-gis-boundaries-london/ESRI/LSOA_2011_London_gen_MHW.shp") %>%
-  dplyr::filter(LAD11NM == "Westminster")
+lsoa <- read_sf(file.path(path,"LSOA_2011_London_gen_MHW.shp")) %>%
+  dplyr::filter(LAD11NM == "Westminster") %>%
+  st_transform(4326)
 
-msoa <- read_sf("temp_data/statistical-gis-boundaries-london/ESRI/MSOA_2011_London_gen_MHW.shp") %>%
-  dplyr::filter(LAD11NM == "Westminster")
+msoa <- read_sf(file.path(path,"MSOA_2011_London_gen_MHW.shp")) %>%
+  dplyr::filter(LAD11NM == "Westminster") %>%
+  st_transform(4326)
 
-wards <- read_sf("temp_data/statistical-gis-boundaries-london/ESRI/London_Ward_CityMerged.shp") %>%
-  dplyr::filter(BOROUGH == "Westminster")
+wards <- read_sf(file.path(path,"London_Ward_CityMerged.shp")) %>%
+  dplyr::filter(BOROUGH == "Westminster") %>%
+  st_transform(4326)
 
-westminster <- read_sf("temp_data/statistical-gis-boundaries-london/ESRI/London_Borough_Excluding_MHW.shp") %>%
-  dplyr::filter(NAME =="Westminster")
+westminster <- read_sf(file.path(path,"London_Borough_Excluding_MHW.shp")) %>%
+  dplyr::filter(NAME =="Westminster") %>%
+  st_transform(4326)
 
 
 #------------ Save filtered Data  ------------#
